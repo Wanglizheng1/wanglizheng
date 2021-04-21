b7441f937b76f2abbe8e62c9745bb2362c641526
Wang Li Zheng <1185815992@qq.com>
2021-04-21 22:22:18 +0800

    day1-2

--- java代码/day2/ChangeChar.java
+++ java代码/day2/ChangeChar.java
@@ -2,11 +2,20 @@
 public class ChangeChar {
     public static void main (String[] args){
         System.out.println("书名\t作者\t价格\t销量\n三国\t罗贯中\t120\t1000");
-/*     \t 制表符对齐功能
-     \n 换行符
-     \r 回车
-     \\ 一个\
-     \" 一个"
+/*  \t 制表符对齐功能
+    \n 换行符
+    \r 回车
+    \\ 一个\
+    \" 一个"
 */
+        //例子=》可读性高
+        //两数相加
+        //定义变量
+        int n1 = 10;
+        int n2 = 30;
+        //求和
+        int sum = n1 + n2;
+        //输出结果
+        System.out.println("结果=" + sum);
     }
 }
---
+++ java代码/day2/Coment01.java
@@ -0,0 +1,12 @@
+//注释使用演示
+
+public class Coment01 {
+    public static void main(String[] args){
+        //单行注释
+        /*多行注释
+        123
+        wang
+        立
+        峥*/
+    }
+}
