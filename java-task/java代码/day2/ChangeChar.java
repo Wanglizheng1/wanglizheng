a5e2ec39b67354499d70f5852e8e80ab9e348b80
Wang Li Zheng <1185815992@qq.com>
2021-04-21 21:05:15 +0800

    day1-2

---
+++ java-task/java代码/day2/ChangeChar.java
@@ -0,0 +1,12 @@
+//  常用转义字符
+public class ChangeChar {
+    public static void main (String[] args){
+        System.out.println("书名\t作者\t价格\t销量\n三国\t罗贯中\t120\t1000");
+/*     \t 制表符对齐功能
+     \n 换行符
+     \r 回车
+     \\ 一个\
+     \" 一个"
+*/
+    }
+}
