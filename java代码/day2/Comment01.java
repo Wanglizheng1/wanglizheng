4774306a8a20470ef731b6af610d49790f83a392
Wang Li Zheng <1185815992@qq.com>
2021-04-22 22:44:39 +0800

    day3

--- java代码/day2/Comment01.java
+++ java代码/day2/Comment01.java
@@ -1,6 +1,6 @@
 //注释使用演示
 
-public class Coment01 {
+public class Comment01 {
     public static void main(String[] args){
         //单行注释
         /*多行注释
---
+++ java代码/day3/Comemnt02.java
@@ -0,0 +1,16 @@
+//注释使用演示
+/**
+ * @ author:wanglizheng
+ * @ version: 1.0
+ */
+public class Comemnt02 {
+    public static void main(String[] args){
+        System.out.println("书名\t作者\t价格\t销量\n三国\t罗贯中\t120\t1000");
+        //单行注释
+        /*多行注释
+        123
+        wang
+        立
+        峥*/
+    }
+}
