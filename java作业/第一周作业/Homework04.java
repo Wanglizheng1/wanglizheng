4916f13f9922a271af21840f83bcacb4906c278c
Wanglizheng1 <77281623+Wanglizheng1@users.noreply.github.com>
2021-04-25 20:49:56 +0800

    Delete Homework04.java

--- java作业/第一周作业/Homework04.java
+++
@@ -1,9 +0,0 @@
-package 第一周作业;
-
-public class Homework04 {
-    /*
-    1.环境变量为了在dos的任意目录使用java和javac命令
-    2.先配置JAVA_HOME = 指向jdk安装的主目录
-    3.编辑path环境变量，增加%AVA_HOME%\bin（%代表引用home的位置）
-     */
-}
