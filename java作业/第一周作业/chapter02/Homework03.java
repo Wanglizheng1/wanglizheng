590ccb4fd7f2f64ef61a7b2934acc989f8ce9885
Wang Li Zheng <1185815992@qq.com>
2021-04-25 20:46:06 +0800

    day3-4

---
+++ java作业/第一周作业/chapter02/Homework03.java
@@ -0,0 +1,8 @@
+package 第一周作业;
+
+public class Homework03 {
+/*
+1. JDK = JRE + java开发工具
+2. JRE = JVM + 核心内库
+ */
+}
---
+++ java作业/第一周作业/chapter2/Homework01.java
@@ -0,0 +1,7 @@
+package 第一周作业;
+
+public class Homework01 {
+    public static void main(String[] args){
+        System.out.println("hello,world");
+    }
+}
