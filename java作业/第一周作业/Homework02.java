94cfc535b5d46424c388c9b1a8bd986e0919f9be
Wang Li Zheng <1185815992@qq.com>
2021-04-26 17:21:20 +0800

    day6--4.25

--- java作业/第一周作业/Homework02.java
+++
@@ -1,8 +0,0 @@
-package 第一周作业;
-
-public class Homework02 {
-    public static void main(String[] args){
-        //  \t制表    \n换行
-        System.out.println("姓名\t性别\t籍贯\t住址\n王立峥\t男\t河北\t北京");
-    }
-}
--- java作业/第一周作业/Homework03.java
+++
@@ -1,8 +0,0 @@
-package 第一周作业;
-
-public class Homework03 {
-/*
-1. JDK = JRE + java开发工具
-2. JRE = JVM + 核心内库
- */
-}
