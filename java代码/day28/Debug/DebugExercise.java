19b26943c995866da25497c1745559fec461799d
Wang Li Zheng <1185815992@qq.com>
2021-05-19 23:56:49 +0800

    day28--5.18

---
+++ java代码/day28/Debug/DebugExercise.java
@@ -0,0 +1,42 @@
+package day28.Debug;
+
+public class DebugExercise {
+    public static void main(String[] args) {
+        Person jack = new Person("JACK", 18);
+        System.out.println(jack);
+
+    }
+}
+class Person{
+    private String name;
+    private int age;
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
